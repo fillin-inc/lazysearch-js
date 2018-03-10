12f8834e84ccb1b84e34a57863030e0798d8f041
Yuya Matsushima <yuyam@fillin-inc.com>
2018-03-10 18:27:46 +0900

    キーワード有無による class 付与処理を追加

--- src/modal.js
+++ src/modal.js
@@ -19,6 +19,7 @@ export default class Modal {
     open() {
         this._el.classList.add('is-active');
         this._setKeywordWidth();
+        this._setHasKeyword();
     }
 
     close() {
@@ -32,11 +33,21 @@ export default class Modal {
     }
 
     _setKeywordWidth() {
-        const target  = this._el.querySelector('[data-lz-form]');
-        const keyword = this._el.querySelector('[data-lz-keyword]');
-        const cross   = this._el.querySelector('[data-lz-x]');
-        const btn     = this._el.querySelector('[data-lz-btn]');
+        const target     = this._el.querySelector('[data-lz-form]');
+        const keyword    = this._el.querySelector('[data-lz-keyword]');
+        const btn        = this._el.querySelector('[data-lz-btn]');
+        const crossWidth = 20;
 
-        keyword.style.width = (target.clientWidth - (cross.clientWidth + btn.clientWidth + 65)).toString() + 'px';
+        keyword.style.width = (target.clientWidth - (crossWidth + btn.clientWidth + 65)).toString() + 'px';
+    }
+
+    _setHasKeyword() {
+        const searchForm = this._el.getElementsByClassName('lz-search-form')[0];
+        const searchKeyword = this._el.querySelector('[data-lz-keyword]');
+        if (searchKeyword.value !== '') {
+            searchForm.classList.add('has-keyword');
+        } else {
+            searchForm.classList.remove('has-keyword');
+        }
     }
 }
