7ea667f8fdb73b1cc26fae0cb3a118f258cde362
Yuya Matsushima <yuyam@fillin-inc.com>
2018-05-29 18:26:22 +0900

    Update modal test

--- test/modal.test.js
+++ test/modal.test.js
@@ -2,14 +2,20 @@ import Modal from '../src/modal';
 import Template from '../src/template';
 
 test('initialize Modal', () => {
-  const el = document.createElement('div');
+  const el = Template.modal();
+  const body = document.getElementsByTagName('body')[0];
+  expect(document.querySelector('[data-lz-modal]')).toBeNull();
+
   const modal = new Modal(el);
-  expect(modal._el).toEqual(el);
-  expect(modal.el()).toEqual(el);
+  expect(modal.el).toEqual(el);
+  expect(modal.body).toEqual(body);
+  expect(modal.form.constructor.name).toBe('ModalForm');
+  expect(modal.results.constructor.name).toBe('ModalResults');
+  expect(document.querySelector('[data-lz-modal]')).not.toBeNull();
 });
 
 test('isVisible() return true when Modal is visible', () => {
-  const el = document.createElement('div');
+  const el = Template.modal();
   const modal = new Modal(el);
 
   el.classList.add('is-active');
@@ -17,15 +23,33 @@ test('isVisible() return true when Modal is visible', () => {
 });
 
 test('isVisible() return false when Modal is invisible', () => {
-  const el = document.createElement('div');
+  const el = Template.modal();
   expect((new Modal(el)).isVisible()).toBe(false);
 });
 
-test('append() innsert Modal element before </body>', () => {
+test('open() display modal', () => {
   const el = Template.modal();
+  const body = document.getElementsByTagName('body')[0];
   const modal = new Modal(el);
 
-  expect(document.querySelector('[data-lz-modal]')).toBeNull();
-  modal.append();
-  expect(document.querySelector('[data-lz-modal]')).not.toBeNull();
+  expect(el.className.indexOf('is-active') >= 0).toBe(false);
+  expect(body.className.indexOf('lz-overflow-hidden') >= 0).toBe(false);
+  expect(modal.open());
+  expect(el.className.indexOf('is-active') >= 0).toBe(true);
+  expect(body.className.indexOf('lz-overflow-hidden') >= 0).toBe(true);
+});
+
+test('close() hide modal', () => {
+  const el = Template.modal();
+  const body = document.getElementsByTagName('body')[0];
+  const modal = new Modal(el);
+
+  modal.el.classList.add('is-active');
+  expect(modal.el.className.indexOf('is-active') >= 0).toBe(true);
+  expect(body.className.indexOf('lz-overflow-hidden') >= 0).toBe(true);
+  modal.close();
+  setTimeout(() => {
+    expect(modal.el.className.indexOf('is-active') >= 0).toBe(false);
+    expect(body.className.indexOf('lz-overflow-hidden') >= 0).toBe(false);
+  }, 200);
 });
