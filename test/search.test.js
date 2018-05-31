87477a489db6f5deeb62b0d58941b2111bf7ce82
Yuya Matsushima <yuyam@fillin-inc.com>
2018-05-31 11:33:07 +0900

    Revert "remove setTimeout"
    This reverts commit ea895d52bf90ef69ead6dd25c45e14502a5d8d1e.

--- test/search.test.js
+++ test/search.test.js
@@ -50,14 +50,11 @@ test('execute() display search result', () => {
   search.execute(btn, baseForm);
 
   return new Promise((resolve, reject) => {
-    while(true) {
-      if (document.querySelectorAll('.lz-result').length > 0) {
-        break;
-      }
-    }
-    expect(document.querySelectorAll('.lz-result').length).toBeGreaterThan(1);
-    expect(document.querySelector('.lz-nav.is-active')).not.toBeNull();
-    resolve();
+    setTimeout(() => {
+      expect(document.querySelectorAll('.lz-result').length).toBeGreaterThan(1);
+      expect(document.querySelector('.lz-nav.is-active')).not.toBeNull();
+      resolve();
+    }, 500);
   });
 });
 
@@ -82,14 +79,11 @@ test('execute() display error when uuid is empty', () => {
   search.execute(btn, baseForm);
 
   return new Promise((resolve, reject) => {
-    while(true) {
-      if (document.querySelectorAll('.lz-result').length > 0) {
-        break;
-      }
-    }
-    expect(document.querySelector('.lz-result').innerHTML).toEqual('param:uuid is empty (4201)');
-    expect(document.querySelector('.has-error')).not.toBeNull();
-    expect(document.querySelector('.lz-nav.is-active')).toBeNull();
-    resolve();
+    setTimeout(() => {
+      expect(document.querySelector('.lz-result').innerHTML).toEqual('param:uuid is empty (4201)');
+      expect(document.querySelector('.has-error')).not.toBeNull();
+      expect(document.querySelector('.lz-nav.is-active')).toBeNull();
+      resolve();
+    }, 500);
   });
 });
