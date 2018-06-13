fbfa3bb25163b8cde80d882cccf7e7d3ea2a1763
Yuya Matsushima <yuyam@fillin-inc.com>
2018-06-13 17:22:34 +0900

    ページナビゲーションをもっと読むに変更 (#26)
    * デザインに「もっと見る」追加
    
    * 表示調整
    
    * ライブラリ側に CSS 反映
    
    * ナビゲーションの表示処理変更
    
    * もっと読むボタンをとりあえず利用できるように
    
    * 検索処理を一旦書き直しに変更
    
    * もっと見るを既存の検索処理から除外
    
    * もっと見る専用イベント処理追加
    
    * ナビゲーションテスト追加
    
    * 「もっと読む」テスト追加
    
    * rename more-read to read-more

--- src/search.js
+++ src/search.js
@@ -40,9 +40,48 @@ export default class Search {
   execute(currentBtn, baseForm) {
     const params = (new SearchParams()).collect(baseForm);
 
-    // 次へ, 前への場合 data-page の値を利用
-    if (currentBtn.parentNode && currentBtn.parentNode.className.indexOf('lz-button') >= 0) {
-      params.page = parseInt(currentBtn.parentNode.dataset.page, 10);
+    if (!params.hasKeyword()) {
+      const results = document.getElementsByClassName('lz-results')[0];
+      const p = document.createElement('p');
+      p.classList.add('lz-result');
+      p.innerHTML = 'サイト内検索にはキーワードの指定が必要です。';
+      results.classList.add('has-error');
+      results.innerHTML = '';
+      results.appendChild(p);
+      document.querySelector('.lz-nav').classList.remove('is-active');
+      return;
+    }
+
+    fetch(this.endpoint + '?' + params.queryString(), {mode: 'cors'})
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((body) => Promise.reject(body));
+        }
+      })
+      .then(this.resolve)
+      .catch(this.reject)
+      .then(this.navigation);
+  }
+
+  /**
+   * 「もっと見る」実行と結果反映
+   *
+   * @param {Element} currentBtn - 押下された検索ボタン DOM
+   * @param {Element} baseForm - 元画面のフォーム DOM
+   */
+  executeReadMore(currentBtn, baseForm) {
+    const keywordInputInModal = document.querySelector('[data-lz-modal] [name=keyword]');
+    const params = (new SearchParams()).collect(baseForm);
+
+    // 前回検索時点のキーワードを設定
+    params.keyword = currentBtn.parentNode.dataset.keyword;
+    params.page = currentBtn.parentNode.dataset.page;
+
+    // 入力欄のキーワードが異なる場合, 元データに変更
+    if (params.keyword !== keywordInputInModal.value.trim()) {
+      keywordInputInModal.value = params.keyword;
     }
 
     if (!params.hasKeyword()) {
@@ -65,7 +104,7 @@ export default class Search {
           return response.json().then((body) => Promise.reject(body));
         }
       })
-      .then(this.resolve)
+      .then(this.resolveReadMore)
       .catch(this.reject)
       .then(this.navigation);
   }
@@ -100,6 +139,40 @@ export default class Search {
 
     results.classList.remove('has-error');
     results.innerHTML = '';
+    results.appendChild(df);
+
+    return body;
+  }
+
+  /**
+   * 「もっと見る」検索結果表示
+   *
+   * 該当するデータが存在しない場合, ページがない旨表示
+   *
+   * @param {Object} body - レスポンス body
+   * @return {Object} - レスポンス body
+   */
+  resolveReadMore(body) {
+    const results = document.getElementsByClassName('lz-results')[0];
+    let df = document.createDocumentFragment();
+
+    if (body.count === 0) {
+      const p = document.createElement('p');
+      p.classList.add('lz-result');
+      p.innerHTML = 'キーワードに該当するページが見つかりませんでした。';
+      df.appendChild(p);
+    } else {
+      body.results.forEach((val) => {
+        let row = Template.result();
+        row.getElementsByTagName('a')[0].href = val.url;
+        row.getElementsByTagName('h3')[0].innerHTML = escapeHtml(val.title);
+        row.getElementsByClassName('url')[0].innerHTML = escapeHtml(val.url);
+        row.getElementsByClassName('desc')[0].innerHTML = val.match;
+        df.appendChild(row);
+      });
+    }
+
+    results.classList.remove('has-error');
     results.appendChild(df);
 
     return body;
