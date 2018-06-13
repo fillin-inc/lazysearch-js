77dc69fc5d662dcf7a89bd88738b83deb7977294
Yuya Matsushima <yuyam@fillin-inc.com>
2018-06-13 16:52:42 +0900

    ナビゲーションテスト追加

--- test/modal/navigation.test.js
+++ test/modal/navigation.test.js
@@ -18,10 +18,29 @@ test('initialize', () => {
 test('update() show navigation', () => {
   const data = {
     has_next: true,
+    current_page: 1,
+    keyword: 'something',
   };
 
   const el = document.querySelector('.lz-nav');
   const form = new ModalNavigation(el);
   form.update(data);
   expect(el.className.indexOf('is-active') >= 0).toBeTruthy();
+  expect(el.dataset.page).toEqual((data.current_page + 1).toString());
+  expect(el.dataset.keyword).toEqual(data.keyword);
+});
+
+test('update() hide navigation', () => {
+  const data = {
+    has_next: false,
+    current_page: 2,
+    keyword: 'something',
+  };
+
+  const el = document.querySelector('.lz-nav');
+  const form = new ModalNavigation(el);
+  form.update(data);
+  expect(el.className.indexOf('is-active') >= 0).toBeFalsy();
+  expect(el.dataset.page).toEqual('1');
+  expect(el.dataset.keyword).toEqual('');
 });
