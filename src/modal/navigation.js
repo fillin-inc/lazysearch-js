105bde3d2b7eaa2b198598d3c67519d7df5657b8
Yuya Matsushima <yuyam@fillin-inc.com>
2018-06-13 11:51:46 +0900

    ナビゲーションの表示処理変更

--- src/modal/navigation.js
+++ src/modal/navigation.js
@@ -11,50 +11,15 @@ export default class ModalNavigation {
    */
   constructor(el) {
     this.el = el;
-    this.nextBtn = this.el.getElementsByClassName('lz-next')[0];
-    this.prevBtn = this.el.getElementsByClassName('lz-prev')[0];
-    this.totalNum = this.el.getElementsByClassName('lz-total-num')[0];
-    this.page = this.el.getElementsByClassName('lz-current-page')[0];
-    this.totalPage = this.el.getElementsByClassName('lz-total-page')[0];
   }
 
   /**
    * ナビゲーション更新
    *
-   * 表示対象の値が 1 つでも存在しなければナビゲーション自体を隠す
-   *
    * @param {Hash} data - ナビゲーション情報
    */
   update(data) {
-    let showFlg = false;
-
-    if (data.count > 0) {
-      this.totalNum.innerHTML = data.count;
-      showFlg = true;
-    }
-
-    if (data.current_page) {
-      this.page.innerHTML = data.current_page;
-      this.totalPage.innerHTML = Math.ceil(data.count / data.per_page);
-    }
-
     if (data.has_next) {
-      this.nextBtn.classList.add('is-active');
-      this.nextBtn.dataset.page = data.current_page + 1;
-    } else {
-      this.nextBtn.classList.remove('is-active');
-      this.nextBtn.dataset.page = 1;
-    }
-
-    if (data.current_page > 1) {
-      this.prevBtn.classList.add('is-active');
-      this.prevBtn.dataset.page = data.current_page - 1;
-    } else {
-      this.prevBtn.classList.remove('is-active');
-      this.prevBtn.dataset.page = data.current_page;
-    }
-
-    if (showFlg) {
       this.el.classList.add('is-active');
     } else {
       this.el.classList.remove('is-active');
--- test/modal/navigation.test.js
+++ test/modal/navigation.test.js
@@ -2,54 +2,26 @@ import ModalNavigation from '../../src/modal/navigation';
 
 beforeEach(() => {
   const html = `<nav class="lz-nav is-active">
-                  <div class="lz-prev lz-button" data-page="1">
-                      <a href="#">&lt;&lt;&nbsp;前へ</a>
-                  </div>
-                  <div class="lz-page-status">
-                      <div>
-                          <span class="lz-current-page">1</span> / <span class="lz-total-page">2</span>
-                      </div>
-                      <div>
-                          <span class="lz-total-num">8</span><span class="lz-num-suffix">件</span>
-                      </div>
-                  </div>
-                  <div class="lz-next lz-button is-active" data-page="2">
-                      <a href="#">次へ&nbsp;&gt;&gt;</a>
-                  </div>
-                </nav>`;
+                  <a id="lz-more-read" class="lz-more-read" href="#">
+                    <span class="more" data-loading="Loading..." data-normal="もっと見る">もっと見る</span>
+                  </a>
+               </nav>`;
   document.body.innerHTML = html;
 });
 
 test('initialize', () => {
   const el = document.querySelector('.lz-nav');
   const form = new ModalNavigation(el);
-
   expect(form.el).toEqual(el);
-  expect(form.nextBtn).not.toBeNull;
-  expect(form.prevBtn).not.toBeNull;
-  expect(form.totalNum).not.toBeNull;
-  expect(form.page).not.toBeNull;
-  expect(form.totalPage).not.toBeNull;
 });
 
 test('update() show navigation', () => {
   const data = {
-    count: 23,
-    current_page: 2,
-    per_page: 5,
     has_next: true,
   };
 
   const el = document.querySelector('.lz-nav');
   const form = new ModalNavigation(el);
   form.update(data);
-
-  expect(form.totalNum.innerHTML).toEqual(data.count.toString());
-  expect(form.page.innerHTML).toEqual(data.current_page.toString());
-  expect(form.totalPage.innerHTML).toEqual('5');
-  expect(form.nextBtn.className.indexOf('is-active') >= 0).toBeTruthy();
-  expect(form.nextBtn.dataset.page).toEqual('3');
-  expect(form.prevBtn.className.indexOf('is-active') >= 0).toBeTruthy();
-  expect(form.prevBtn.dataset.page).toEqual('1');
-  expect(form.el.className.indexOf('is-active') >= 0).toBeTruthy();
+  expect(el.className.indexOf('is-active') >= 0).toBeTruthy();
 });
